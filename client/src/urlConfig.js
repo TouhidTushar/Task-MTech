14aa6af176820661bcdc1bbf9101680f04ef04e8
TouhidTushar <mohd.touhedul.alam@g.bracu.ac.bd>
2021-06-27 01:54:54 +0600

    update26-06-21

--- client/src/urlConfig.js
+++ client/src/urlConfig.js
@@ -1,3 +1,3 @@
 /*****url config for devlopment*****/
 export const api = "http://localhost:2000/api";
-export const rootDir = "http://localhost:3000";
+export const rootDir = "http://localhost:2000";
